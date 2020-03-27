f0b7c5919425f86335b06c0ec72b3d06e546898e
serkan uz <serkanuz@serkans-MacBook-Pro.local>
2020-03-27 16:31:12 +0000

    database

--- python/db.py
+++ python/db.py
@@ -74,7 +74,6 @@ class DbOps(object):
 
 
     def execute_sql_return_results(self, sql):
-        print(sql)
         cnx = self.create_connection()
         cur = cnx.cursor()
         select_statement = sql
