ccb8d3f99530fb7887d54bafa9ed2d4620c9beb2
serkan uz <serkanuz@serkans-MacBook-Pro.local>
2020-07-05 02:25:00 +0100

    datapare

--- node/mysql.js
+++ node/mysql.js
@@ -2,67 +2,26 @@ const mysql = require('mysql');
 
 const config = require('../config.json');
 
-
-
-function MySQLHelper (db="default_db") {
-
-    const mySQLConfig = config[db];
-    this.con = mysql.createConnection({
-        host: mySQLConfig.host,
-        port: mySQLConfig.port,
-        user: mySQLConfig.user,
-        password: mySQLConfig.password,
-        database: mySQLConfig.database
-      });
-      console.log('[MySQLHelper] ---  Succesfully created connection --- ')
-};
-
-
-MySQLHelper.prototype.get = function (query) {
-    var self = this;
-    return new Promise(function (resolve, reject) {
-        self.con.connect(function(err) {
-            if (err) return err;
-            self.con.query(query, function (err, result, fields) {
-                if (err) throw err;
-                self.con.end();
-                resolve(result);
-            });
+class MySQLHelper {
+    constructor(db="default_db"){
+        this.mySQLConfig = config[db];
+        this.con = mysql.createConnection({
+            host: this.mySQLConfig.host,
+            user: this.mySQLConfig.user_name,
+            password: this.mySQLConfig.password,
+            database: this.mySQLConfig.db_name
           });
-    });
-};
+    }
 
-MySQLHelper.prototype.execute_sql = function (query) {
-    var self = this;
-    console.log(query);
-    return new Promise(function (resolve, reject) {
-        self.con.connect(function(err) {
-            if (err) return err;
-            console.log('connection established');
-            self.con.query(query, function (err, result, fields) {
-                if (err) throw err;
-                self.con.end();
-                console.log('query done');
-                resolve(result);
+    async executeSql(query){
+        var self = this;
+        return new Promise(async (resolve, reject) => {
+            await this.con.query(query, (err,rows) => {
+                if(err) throw err;
+                resolve(rows);
             });
-          });
-    });
-};
-
-MySQLHelper.prototype.insert = function (query, data) {
-    var self = this;
-    return new Promise(function (resolve, reject) {
-        self.con.connect(function(err) {
-            if (err) return err;
-            self.con.query(query, [data], function (err, result, fields) {
-                if (err) throw err;
-                self.con.end();
-                resolve(result);
-            });
-          });
-    });
-};
-
-
+        });
+    }
+}
 
 module.exports = MySQLHelper;
