ff0a548d590992e45416fab76985bc34cdd41cc0
Baran Berkay Barakçin <baranberkaybarakcin@Baran-MacBook-Pro.local>
2020-08-04 20:32:49 +0300

    upgrade to new blob azure

--- python/azure_blob.py
+++ python/azure_blob.py
@@ -10,29 +10,34 @@ from azure.storage.blob import BlobServiceClient
 
 class BlobOps(object):
     fs = filesystem.FileSystemOps()
-    def __init__(self, storage_account="azure", **kwargs):
+    def __init__(self, storage_account="trackstorage", **kwargs):
         self.config = self.fs.read_json_from_file(os.path.dirname(__file__) + '/../config.json')
-        self.blob = BlobServiceClient(account_url="https://{account_name}.blob.core.windows.net".format(account_name=self.config[storage_account]["storage_account"]),
-                                        credentials=self.config[storage_account]["storage_account_key"]) 
+        print(self.config)
+        self.blob_service = BlobServiceClient.from_connection_string(self.config[storage_account]["connection_string"])
     
     def create_container(self, container_name):
-        self.blob.create_container(container_name) 
-        #self.blob.set_container_acl(container_name, public_access=ContainerPermissions.READ)
+        self.blob_service.create_container(container_name)
+
 
     def upload_file(self, container_name, file_name, full_path):
-        self.blob.create_blob_from_path(container_name, file_name, full_path)
-
+        blob_client = self.blob_service.get_blob_client(container=container_name, blob=file_name)
+        with open(full_path, "rb") as data:
+            blob_client.upload_blob(data)
     
     def container_file_list(self, container_name):
-        generator = self.blob.list_blobs(container_name)
+        container = self.blob_service.get_container_client(container=container_name)
+        generator = container.list_blobs()
         return generator
     
     def download_file(self, container_name, file_name, full_path):
-        self.blob.get_blob_to_path(container_name, file_name, full_path)
+        blob_client = self.blob_service.get_blob_client(container=container_name, blob=file_name)
+        with open(full_path, "wb") as f:
+            f.writelines([blob_client.download_blob().readall()])
 
     def delete_container(self, container_name):
-        self.blob.delete_container(container_name)
+        self.blob_service.delete_container(container_name)
 
     def delete_file(self, container_name, blob_name):
-        self.blob.delete_blob(container_name, blob_name)
+        blob_client = self.blob_service.get_blob_client(container=container_name, blob=blob_name)
+        blob_client.delete_blob()
 
