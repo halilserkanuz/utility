d43c874d64e5b5792f0ae7a4b784eb5b35b17ce1
serkan uz <serkanuz@serkans-MacBook-Pro.local>
2020-09-21 00:44:20 +0100

    postgresql implemented

---
+++ node/db.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql');
+const { Client } = require('pg');
+
+const config = require('../config.json');
+
+class DBHelper {
+    constructor(db="default_db"){
+        this.dbConfig = config[db];
+        if (this.dbConfig.db_type =="mysql") {
+            this.con = mysql.createConnection({
+                host: this.dbConfig.host,
+                user: this.dbConfig.user_name,
+                password: this.dbConfig.password,
+                database: this.dbConfig.db_name
+              });
+        } else if (this.dbConfig.db_type =="postgresql") {
+            this.con = new Client({
+                host: this.dbConfig.host,
+                user: this.dbConfig.user_name,
+                password: this.dbConfig.password,
+                database: this.dbConfig.db_name,
+                port: this.dbConfig.port
+            });
+            console.log(this.dbConfig);
+            this.con.connect();
+        }
+        
+    }
+
+    async executeSql(query){
+        var self = this;
+        return new Promise(async (resolve, reject) => {
+            await this.con.query(query, (err,res) => {
+                if(err) {
+                    throw err;
+                }
+                if (this.dbConfig.db_type =="mysql") {
+                    resolve(res);
+                } else if (this.dbConfig.db_type =="postgresql") {
+                    resolve(res.rows);
+                }
+                
+            });
+        });
+    }
+}
+
+module.exports = DBHelper;
