4bfc2af3be008d10bf3ca7b5d2a21855877b31a8
serkan uz <serkanuz@serkans-MacBook-Pro.local>
2021-04-25 00:14:12 +0100

    Mongo helper

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .src.redis import RedisHelper
 from .src.db import DBHelper
-
+from .src.mongo import MongoHelper
---
+++ src/mongo.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+import os
+
+
+
+class MongoHelper(object):
+
+    def __init__(self, db_name="datapare"):
+        self.db_name = db_name
+
+    def create_client(self):
+        client = MongoClient('mongodb://{0}:{1}@{2}'.format(os.getenv("MONGO_USER"), os.getenv("MONGO_PASSWORD"), os.getenv("MONGO_HOST")))
+        return client[self.db_name]
+
+    def insert(self, collection: str, data: dict) -> None:
+        db = self.create_client()
+        _collection = db[collection]
+        _collection.insert_one(data)
+
+    def query(self,  collection: str, data: dict) -> None:
+        db = self.create_client()
+        _collection = db[collection]
+        return _collection.find(data)
